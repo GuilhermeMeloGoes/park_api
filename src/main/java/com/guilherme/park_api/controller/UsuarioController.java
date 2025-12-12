967a417f557680f184059e51d3b043acae0f1d58
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2025-12-12 10:52:27 -0300

    adicionando recursos para o usuario

--- src/main/java/com/guilherme/park_api/controller/UsuarioController.java
+++ src/main/java/com/guilherme/park_api/controller/UsuarioController.java
@@ -1,9 +1,13 @@
 package com.guilherme.park_api.controller;
 
+import com.guilherme.park_api.entity.Usuario;
 import com.guilherme.park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioCriado = usuarioService.cadastrarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioCriado);
+    }
+
+    @GetMapping("/buscar/id/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
+        Usuario usuarioRecuperadoPorId = usuarioService.buscarUsuarioPorId(id);
+        return ResponseEntity.ok(usuarioRecuperadoPorId);
+    }
+
+    @PatchMapping("/atualizar/{id}")
+    public ResponseEntity<Usuario> atualizarSenhaUsuarioPorId(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario usuarioAtualizado = usuarioService.editarSenhaPorId(id, usuario);
+        return ResponseEntity.ok(usuarioAtualizado);
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<Usuario>> buscarUsuarios() {
+        List<Usuario> usuarios = usuarioService.buscarUsuarios();
+        return ResponseEntity.ok(usuarios);
+    }
+
 }
--- src/main/java/com/guilherme/park_api/service/UsuarioService.java
+++ src/main/java/com/guilherme/park_api/service/UsuarioService.java
@@ -1,12 +1,42 @@
 package com.guilherme.park_api.service;
 
+import com.guilherme.park_api.entity.Usuario;
 import com.guilherme.park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+    }
+
+    @Transactional
+    public Usuario editarSenhaPorId(Long id, Usuario usuario) {
+        Usuario usuarioAtual = this.buscarUsuarioPorId(id);
+        usuarioAtual.setPassword(usuario.getPassword());
+        return usuarioAtual;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarUsuarios() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        if (usuarios.isEmpty()) {
+            return null;
+        }
+        return usuarios;
+    }
 }
